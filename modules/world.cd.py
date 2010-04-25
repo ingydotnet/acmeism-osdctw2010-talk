c8811c5173af65eb11132cd4d40823c79ba0a5c9
Ingy dot Net <ingy@ingy.net>
2010-04-24 19:46:09 -0700

    modules

---
+++ modules/world.cd.py
@@ -0,0 +1,7 @@
+"""\
+This is World class :)
+"""
+
+class World():
+    def greet(self):
+        print "Hello, world"
---
+++ modules/world.py
@@ -0,0 +1,8 @@
+# *** DO NOT EDIT ***  This is a C'Dent generated Python module.
+"""\
+This is World class :)
+"""
+
+class World():
+    def greet(self):
+        print "Hello, world"        
